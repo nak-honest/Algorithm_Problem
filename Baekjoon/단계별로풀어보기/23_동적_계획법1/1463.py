6da0500fdbd106d54722d2531067172a8eb2df87
nhlee98 <e5slnh9876@gmail.com>
2023-06-20 14:50:27 +0900

    Add: 1463 문제 DP 풀이법 추가

--- Baekjoon/단계별로풀어보기/23_동적_계획법1/1463.py
+++ Baekjoon/단계별로풀어보기/23_동적_계획법1/1463.py
@@ -1,6 +1,5 @@
 # DP 문제라고 소개되어 있는데 백트래킹으로 풀리긴 했다.
 # 하지만 DP로도 풀어보자.
-
 min_count = 1000000
 
 
@@ -33,3 +32,48 @@ N = int(input())
 make_one(N, 0)
 print(min_count)
 
+
+'''
+# bottom-up 방식
+# 나는 DP로 접근할때 계속 연산의 결과를 저장시키도록 풀었는데, 연산 횟수를 저장하도록 풀어야 한다.
+N = int(input())
+
+DP = [0] * (N+1) # DP[i]는 i를 1로 만드는데 필요한 연산의 최소값을 저장한다.
+
+for i in range(2, N+1):
+    DP[i] = DP[i-1] + 1
+    if i % 2 == 0:
+        DP[i] = min(DP[i], DP[i//2] + 1)
+    if i % 3 == 0:
+        DP[i] = min(DP[i], DP[i//3] + 1)
+
+print(DP[N])
+'''
+
+'''
+# top-down 방식
+# 가장 빠르게 풀린다.
+N = int(input())
+
+DP = [0] * (N+1)
+def make_one(n):
+    if n == 1:
+        return 0
+    if DP[n] != 0:
+        return DP[n]
+
+    if n % 2 != 0 and n % 3 != 0:
+        DP[n] = make_one(n-1) + 1
+    elif n % 2 != 0:
+        DP[n] = min(make_one(n-1), make_one(n//3)) + 1
+    elif n % 3 != 0:
+        DP[n] = min(make_one(n-1), make_one(n//2)) + 1
+    else:
+        DP[n] = min(make_one(n//2), make_one(n//3)) + 1
+
+    return DP[n]
+
+
+make_one(N)
+print(DP[N])
+'''
